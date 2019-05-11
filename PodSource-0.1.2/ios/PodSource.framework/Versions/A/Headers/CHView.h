076be21643d521dc47e14f68575f69ac38274b2c
f27_100008550770_yongsheng <f27_100008550770_yongsheng@git.cloud.tencent.com>
2019-05-11 10:07:59 +0800

    0.1.3

---
+++ PodSource-0.1.2/ios/PodSource.framework/Versions/A/Headers/CHView.h
@@ -0,0 +1,16 @@
+//
+//  CHView.h
+//  Pods-PodSource_Example
+//
+//  Created by 李长鸿 on 2019/5/4.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface CHView : UIView
+@property (nonatomic,strong)UILabel *label;
+@end
+
+ 
